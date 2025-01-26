67f595e04da247c65b5e2973ffd7a72184b8fbe1
Bogdan <bogdan.g.branescu@gmail.com>
2025-01-26 14:25:37 +0200

    add music playback functionality

--- world/World.gd
+++ world/World.gd
@@ -6,7 +6,10 @@ var current_hp := 20#Global.MAX_HP
 
 
 func _ready() -> void:
-    pass
+    $BGMPeaceful.play()
+
+    $BGMAttack.volume_db = -80
+    $BGMAttack.play()
 
 
 func _process(_delta: float) -> void:
@@ -16,3 +19,11 @@ func _process(_delta: float) -> void:
 func heal(amount: int) -> void:
     current_hp = clamp(current_hp + amount, 0, 100)
     %HealthBar.value = current_hp
+
+
+func crossfade_music(from: AudioStreamPlayer2D, to: AudioStreamPlayer2D):
+    var tween_fade_out = get_tree().create_tween()
+    var tween_fade_in = get_tree().create_tween()
+
+    tween_fade_out.tween_property(from, "volume_db", -80, 2)
+    tween_fade_in.tween_property(to, "volume_db", 0, 2)
