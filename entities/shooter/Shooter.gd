98e410cc0b34ea2a27f28b1234c7fb8daf578420
Bogdan <bogdan.g.branescu@gmail.com>
2025-01-26 16:17:16 +0200

    bullet audio

--- entities/shooter/Shooter.gd
+++ entities/shooter/Shooter.gd
@@ -4,19 +4,19 @@ var type := Global.TILE_TYPE.SHOOTER
 
 
 func interact_on_collision():
-    print("Shooter recieved bubble")
-    shootAtOctopus()
-    pass
+	print("Shooter recieved bubble")
+	shootAtOctopus()
+	pass
 
 
 func shootAtOctopus():
-    print("Shooter shooting at octopus")
-    make_bullet()
+	print("Shooter shooting at octopus")
+	make_bullet()
+	$SFX.play()
 
-
-    pass
+	pass
 
 
 func make_bullet():
-    var tilemap_position = get_parent().local_to_map(position)
-    get_parent().place_tile(tilemap_position + Vector2i.UP, Global.TILE_TYPE.BULLET)
+	var tilemap_position = get_parent().local_to_map(position)
+	get_parent().place_tile(tilemap_position + Vector2i.UP, Global.TILE_TYPE.BULLET)
