1ae1ecd88d1ad20652ed947f1f6351e32706fc2e
WillYum <liamshere98@gmail.com>
2025-01-28 01:16:10 +0200

    Handle changing sprites when shooters & collectors run an action

--- entities/collector/Collector.gd
+++ entities/collector/Collector.gd
@@ -6,3 +6,10 @@ var type := Global.TILE_TYPE.COLLECTOR
 
 func interact_on_collision():
 	world.current_bubbles += Global.BUBBLES_PER_COLLECTOR
+	handle_play_on_shoot()
+
+
+func handle_play_on_shoot():
+	$"AnimatedSprite2D".frame = 1
+	await get_tree().create_timer(0.5).timeout
+	$"AnimatedSprite2D".frame = 0
