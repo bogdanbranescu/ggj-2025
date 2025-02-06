f198bc01d083cd27f1c3d499bded3120e70d04c7
Bogdan <bogdan.g.branescu@gmail.com>
2025-02-06 12:49:03 +0200

    improve placement and icon

--- ui/ui_elements/Cursor.gd
+++ ui/ui_elements/Cursor.gd
@@ -9,9 +9,12 @@ extends Area2D
 }
 
 var type: int
-var cost_of_type:int;
+var cost_of_type: int;
+var initial_position := position
 
 @onready var world = get_node(Global.world_path)
+@onready var tilemap = %TileMapEntities as TileMapLayer
+
 
 func _ready() -> void:
 	toggle_behavior(false)
@@ -33,7 +36,7 @@ func _input(_event: InputEvent) -> void:
 		direction = Vector2.RIGHT
 
 	if direction != Vector2.ZERO:
-		var next_position = %TileMapEntities.local_to_map(self.position + direction * Global.TILE_SIZE)
+		var next_position = tilemap.local_to_map(self.position + direction * Global.TILE_SIZE)
 		if next_position.x == clamp(next_position.x, 2, 15) and next_position.y == clamp(next_position.y, 2, 11):
 			position += direction * Global.TILE_SIZE
 
@@ -41,20 +44,17 @@ func _input(_event: InputEvent) -> void:
 		match type:
 			Global.ITEM_TYPE.SHOVEL:
 				dig()
-				$SFX.play()
-	
 			Global.ITEM_TYPE.COLLECTOR:
-				place_collector()
-				$SFX.play()
-		
+				place_item()
 			Global.ITEM_TYPE.SHOOTER:
-				place_shooter()
-				$SFX.play()
+				place_item()
 	
 	if Input.is_action_just_pressed("cancel"):
 		toggle_behavior(false)
 		world.current_bubbles += cost_of_type
 		%StateChart.send_event("go_to_shop")
+
+	adjust_icon()
 
 
 func _on_placement_state_entered() -> void:
@@ -69,58 +69,77 @@ func toggle_behavior(value: bool) -> void:
 		set_process(value)
 		set_process_input(value)
 
+		position = initial_position
+
 		if value:
 			show()
 		else:
 			hide()
 
+
 func set_type(cursor_type: int, price: int) -> void:
 	type = cursor_type
 	cost_of_type = price
-	$Sprite.frame = type
+	adjust_icon()
+
+
+func adjust_icon():
+	var map_position = tilemap.local_to_map(self.position)
+	if is_current_item_placeable(map_position):
+		$Sprite.frame = type
+	else:
+		$Sprite.frame = type + 4
 
 
 func dig():
-	var tileMapEnts: TileMapLayer = %TileMapEntities;
+	var map_position = tilemap.local_to_map(self.position)
+	if not is_current_item_placeable(map_position):
+		return
 
-	var map_position = tileMapEnts.local_to_map(self.position)
-	var cell_data = tileMapEnts.get_cell_tile_data(map_position)
-	var cell_data_above = tileMapEnts.get_cell_tile_data(map_position + Vector2i.UP)
+	tilemap.remove_tile(map_position)
+	$SFX.play()
+	%StateChart.send_event("end_placement")
 
-	var is_sand = cell_data and cell_data.get_custom_data("is_sand")
-	var has_sand_above = cell_data_above and cell_data_above.get_custom_data("is_sand")
+	if world.checkIfCanSpawnClam():
+		tilemap.place_tile(map_position, Global.TILE_TYPE.SHELL)
+	else:
+		tilemap.place_tile(map_position, Global.TILE_TYPE.BUBBLE)
+
+
+func place_item():
+	var map_position = tilemap.local_to_map(self.position)
+	if not is_current_item_placeable(map_position):
+		return
+	
+	tilemap.place_tile(map_position, type)
+	$SFX.play()
+	%StateChart.send_event("end_placement")
 	
 
-	var currentCellId = tileMapEnts.get_cell_source_id(map_position + Vector2i.UP);
-	print("currentCellId: ", currentCellId);
-	var shell_id = 0;
-	var has_clam_at_top = currentCellId == shell_id;
-
-	if is_sand and not has_sand_above && !has_clam_at_top:
-		tileMapEnts.remove_tile(map_position)
-		%StateChart.send_event("end_placement")
-
-		if world.checkIfCanSpawnClam():
-			tileMapEnts.place_tile(map_position, Global.TILE_TYPE.SHELL)
-		else:
-			tileMapEnts.place_tile(map_position, Global.TILE_TYPE.BUBBLE)
-
-
-func place_shooter():
-	var map_position = %TileMapEntities.local_to_map(self.position)
-	var cell_data = %TileMapEntities.get_cell_tile_data(map_position)
+func is_current_item_placeable(map_position: Vector2i) -> bool:
+	var cell_data = tilemap.get_cell_tile_data(map_position)
+	var cell_data_above = tilemap.get_cell_tile_data(map_position + Vector2i.UP)
+	var cell_data_below = tilemap.get_cell_tile_data(map_position + Vector2i.DOWN)
+	
 	var is_sand = cell_data and cell_data.get_custom_data("is_sand")
+	var has_sand_above = cell_data_above and cell_data_above.get_custom_data("is_sand")
+	var has_sand_below = cell_data_below and cell_data_below.get_custom_data("is_sand")
+	
+	var shell_id = 3 # uses alternative_tile in Scene Collections
+	var tile_type_above = tilemap.get_cell_alternative_tile(map_position + Vector2i.UP)
+	var has_shell_above = tile_type_above == shell_id
+	
+	var tile_type_below = tilemap.get_cell_alternative_tile(map_position + Vector2i.DOWN)
+	var has_shell_below = tile_type_below == shell_id
+	
+	match type:
+		Global.ITEM_TYPE.SHOVEL:
+			return is_sand and not has_sand_above && !has_shell_above
 
-	if not is_sand:
-		%TileMapEntities.place_tile(map_position, Global.TILE_TYPE.SHOOTER)
-		%StateChart.send_event("end_placement")
+		Global.ITEM_TYPE.COLLECTOR:
+			return !is_sand and !has_sand_below and !has_shell_below
 
+		Global.ITEM_TYPE.SHOOTER:
+			return !is_sand and !has_sand_below and !has_shell_below
 
-func place_collector():
-	var map_position = %TileMapEntities.local_to_map(self.position)
-	var cell_data = %TileMapEntities.get_cell_tile_data(map_position)
-	var is_sand = cell_data and cell_data.get_custom_data("is_sand")
-
-	if not is_sand:
-		%TileMapEntities.place_tile(map_position, Global.TILE_TYPE.COLLECTOR)
-		%StateChart.send_event("end_placement")
+	return true
